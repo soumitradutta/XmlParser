ef7e6ff244d7ffc85c606f71e4e7a1306edd5821
soumitradutta <ai.soumitra@gmail.com>
2020-03-09 21:42:59 +0530

    4th

--- src/org/xmlparser/XmlParseHelper.java
+++ src/org/xmlparser/XmlParseHelper.java
@@ -1,24 +1,135 @@
 package org.xmlparser;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Text;
+import org.xml.sax.SAXException;
 
 //https://www.journaldev.com/901/modify-xml-file-in-java-dom-parser
+//https://www.w3schools.com/xml/xml_attributes.asp
 public final class XmlParseHelper {
 	
-	public static final void addElement(final Document doc) {
+	public static final void showEmployeeDetailsById(final Document doc, final String empId) {
+		final NodeList employees = (NodeList) doc.getElementsByTagName("Employee");
+		Element employee = null;
+		NodeList empDetailsList = null;
+		Node empDetailNode = null;
+		Node empDetailNodeValue = null;
 		
+		if (employees != null) {
+			for (int len = 0; len < employees.getLength(); len++) {
+				employee = (Element) employees.item(len);
+				if (employee != null && empId.equals(employee.getAttribute("id"))) {
+					empDetailsList = employee.getChildNodes();
+					for (int index = 0; index < empDetailsList.getLength(); index++) {
+						empDetailNode = empDetailsList.item(index);
+						if (empDetailNode != null && empDetailNode.getNodeType() == Node.ELEMENT_NODE) {
+							if (empDetailNode.hasChildNodes()) {
+								empDetailNodeValue = empDetailNode.getFirstChild();
+							} else {
+								empDetailNodeValue = null;
+							}
+							System.out.println(empDetailNode.getNodeName() + "  :  " + (empDetailNodeValue != null ? empDetailNodeValue.getTextContent() : null));
+						}
+					}
+				}
+			}
+		}
 	}
 	
-	public static final void deleteElement(final Document doc) {
-		
+	public static final void addElement(final Document doc, final String empId, final String newElementName, final String newElementValue) {
+		final NodeList employees = (NodeList) doc.getElementsByTagName("Employee");
+		Element employee = null;
+		if (null != employees) {
+			for (int len = 0; len < employees.getLength(); len ++) {
+				employee = (Element) employees.item(len);
+				if (null != employee && empId.equals(employee.getAttribute("id"))) {
+					final Element newElement = doc.createElement(newElementName);
+					final Text textNode = doc.createTextNode(newElementValue);
+					newElement.appendChild(textNode);
+					employee.appendChild(newElement);
+				}
+			}
+		}
 	}
 	
-	public static final void updateElementValue(final Document doc) {
-		
+	public static final void deleteElement(final Document doc, final String empId, final String elementToBeRemoved) {
+		final NodeList employees = (NodeList) doc.getElementsByTagName("Employee");
+		Element employee = null;
+		if (null != employees) {
+			for (int len = 0; len < employees.getLength(); len ++) {
+				employee = (Element) employees.item(len);
+				if (null != employee && empId.equals(employee.getAttribute("id"))) {
+					final Node nodeToBeRemoved = employee.getElementsByTagName(elementToBeRemoved).item(0);
+					employee.removeChild(nodeToBeRemoved);
+				}
+			}
+		}
 	}
 	
-	public static final void updateAttributeValue(final Document doc) {
-		
+	public static final void updateElementValue(final Document doc, final String empId, final String elementName, final String elementValue) {
+		final NodeList employees = (NodeList) doc.getElementsByTagName("Employee");
+		Element employee = null;
+		if (null != employees) {
+			for (int len = 0; len < employees.getLength(); len ++) {
+				employee = (Element) employees.item(len);
+				if (null != employee && empId.equals(employee.getAttribute("id"))) {
+					final Text textNodeToBeUpdated = (Text) employee.getElementsByTagName(elementName).item(0).getFirstChild();
+					textNodeToBeUpdated.setNodeValue(elementValue);
+				}
+			}
+		}
+	}
+	
+	public static final void updateAttributeValue(final Document doc, final String empId, final String newAttrValue) {
+		final NodeList employees = (NodeList) doc.getElementsByTagName("Employee");
+		Element employee = null;
+		if (null != employees) {
+			for (int len = 0; len < employees.getLength(); len ++) {
+				employee = (Element) employees.item(len);
+				if (null != employee && empId.equals(employee.getAttribute("empId"))) {
+					employee.setAttribute("id", newAttrValue);
+				}
+			}
+		}
+	}
+	
+	public static final void mergeXml(final Document doc, final File sourceFile) {
+		final Element employees = doc.getDocumentElement();
+		final Document sourceDoc = newDocumentInstance(sourceFile);
+		final NodeList empsFromSource = (NodeList) sourceDoc.getElementsByTagName("Employee");
+		Node empToBeAppended = null;
+		if (employees != null && empsFromSource != null) {
+			for (int len = 0; len < empsFromSource.getLength(); len ++) {
+				empToBeAppended = empsFromSource.item(len);
+				employees.appendChild(empToBeAppended);
+			}
+		}
+	}
+	
+	public static final Document newDocumentInstance(final File file) {
+		Document document = null;
+        try {
+        	final DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+			final DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+			document = docBuilder.parse(file);
+		} catch (final ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (final SAXException e) {
+			e.printStackTrace();
+		} catch (final IOException e) {
+			e.printStackTrace();
+		} 
+        return document;
 	}
 
 }
--- src/org/xmlparser/XmlParser.java
+++ src/org/xmlparser/XmlParser.java
@@ -12,10 +12,18 @@ import java.io.IOException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 public class XmlParser {
 	
     private final static String FILE_LOCATION = "C:\\workspace_2\\XmlParserTest\\XmlParser\\src\\org\\xmlparser\\TestFile.xml";
+    private final static String SOURCE_FILE = "C:\\workspace_2\\XmlParserTest\\XmlParser\\src\\org\\xmlparser\\source.xml";
 
     private void printValuesLikeMap(final Document doc) {
         final Element element = doc.getDocumentElement();
@@ -58,20 +66,29 @@ public class XmlParser {
 
     private void processXml() {
         try {
-            final DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
-            final DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
-            final Document doc = docBuilder.parse(new File(FILE_LOCATION));
+        	final File xmlFile = new File(FILE_LOCATION);
+            final Document doc = XmlParseHelper.newDocumentInstance(xmlFile);
             //printValuesLikeMap(doc);
             //m1(doc);
+            //XmlParseHelper.showEmployeeDetailsById(doc, "2");
+            //XmlParseHelper.addElement(doc, "2", "salary", "121212");
+            //XmlParseHelper.deleteElement(doc, "2", "salary");
+            //XmlParseHelper.updateElementValue(doc, "2", "role", "Java Developer");
+            //XmlParseHelper.updateAttributeValue(doc, "1", "5");
+            XmlParseHelper.mergeXml(doc, new File(SOURCE_FILE));
+            
+            final TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            final Transformer transformer = transformerFactory.newTransformer();
+            final DOMSource source = new DOMSource(doc);
+            final StreamResult result = new StreamResult(xmlFile);
+            transformer.setOutputProperty(OutputKeys.INDENT, "no");
+            transformer.transform(source, result);
 
-        } catch (final ParserConfigurationException e) {
-            e.printStackTrace();
-        } catch (final SAXException e) {
-            e.printStackTrace();
-        } catch (final IOException e) {
-            e.printStackTrace();
-        }
-
+        } catch (final TransformerConfigurationException e) {
+			e.printStackTrace();
+		} catch (final TransformerException e) {
+			e.printStackTrace();
+		}
     }
 
 	public static void main(final String args[]) {
